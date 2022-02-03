c31a8dd7e6c79139fe20225e9afff75e3ce6aae4
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2022-02-03 22:32:39 +0900

    BOJ 11726

---
+++ BaekJoon/다이나믹 프로그래밍/BOJ 11726.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ BOJ 11726 2xN 타일링
+ */
+public class Main {
+    static int n;
+    static int[] dp;
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n + 2];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] % 10007;
+        }
+        System.out.println(dp[n]%10007);
+    }
+}
