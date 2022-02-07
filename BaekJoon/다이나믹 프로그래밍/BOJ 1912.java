c0f766b77f326272fc52e5a204f0c759e9810d0d
taeukyoon <79095857+taeukyoon@users.noreply.github.com>
2022-02-07 20:10:52 +0900

    BOJ 1912

---
+++ BaekJoon/다이나믹 프로그래밍/BOJ 1912.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ BOJ 1912 연속합
+ */
+public class Main {
+    static int n;
+    static int[] arr, dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1];
+        dp = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = Integer.MIN_VALUE;
+        dp[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}
